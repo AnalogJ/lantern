2466597d95469371749f726c176a73d16f7708ac
Jason Kulatunga <jason@thesparktree.com>
2019-04-03 17:12:01 -0700

    added addl TODO.

--- common/models/db_notify.go
+++ common/models/db_notify.go
@@ -3,6 +3,6 @@ package models
 type DbNotify struct {
 	Table        string `json:"table"`
 	Action	 	 string `json:"action"`
-	Data 		map[string]interface{} `json:"data"`
+	Id 		int `json:"id"`
 }
 
