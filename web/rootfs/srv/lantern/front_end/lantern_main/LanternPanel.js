4058875a748979b969910dee2c53cf21dce0c328
Jason Kulatunga <jason@thesparktree.com>
2019-04-07 22:06:13 -0700

    cleanup view panel functions.

--- web/rootfs/srv/lantern/front_end/lantern_main/LanternPanel.js
+++ web/rootfs/srv/lantern/front_end/lantern_main/LanternPanel.js
@@ -13,8 +13,6 @@ LanternMain.LanternPanel = class extends UI.Panel {
     const image = container.createChild('img', 'node-panel-logo');
     image.src = '/logo.png';
 
-    InspectorFrontendHost.events.addEventListener(
-        InspectorFrontendHostAPI.Events.DevicesDiscoveryConfigChanged, this._devicesDiscoveryConfigChanged, this);
 
     /** @type {!Adb.Config} */
     this._config;
@@ -22,23 +20,12 @@ LanternMain.LanternPanel = class extends UI.Panel {
     this.contentElement.tabIndex = 0;
     this.setDefaultFocusedElement(this.contentElement);
 
-    // Trigger notification once.
-    InspectorFrontendHost.setDevicesUpdatesEnabled(false);
-    InspectorFrontendHost.setDevicesUpdatesEnabled(true);
 
     this._networkDiscoveryView = new LanternMain.LanternView(config => {
       this._config.networkDiscoveryConfig = config;
       InspectorFrontendHost.setDevicesDiscoveryConfig(this._config);
     });
     this._networkDiscoveryView.show(container);
-  }
-
-  /**
-   * @param {!Common.Event} event
-   */
-  _devicesDiscoveryConfigChanged(event) {
-    this._config = /** @type {!Adb.Config} */ (event.data);
-    this._networkDiscoveryView.discoveryConfigChanged(this._config.networkDiscoveryConfig);
   }
 };
 
@@ -61,113 +48,24 @@ LanternMain.LanternView = class extends UI.VBox {
     link.textContent = Common.UIString('Learn more');
     link.addEventListener('click', () => InspectorFrontendHost.openInNewTab('https://github.com/analogj/lantern'));
 
-    const addButton = UI.createTextButton(
+    const inspectButton = UI.createTextButton(
         Common.UIString('Inspect HTTPS Traffic'), this._downloadCACertificateButtonClicked.bind(this), 'add-network-target-button',
         true /* primary */);
-    this.element.appendChild(addButton);
+    this.element.appendChild(inspectButton);
 
-    /** @type {!Array<{address: string}>} */
-    this._networkDiscoveryConfig = [];
+
+    const mobileConfigButton = UI.createTextButton(
+        Common.UIString('IOS MobileConfig'), this._downloadMobileConfigButtonClicked.bind(this), 'add-network-target-button',
+        true /* primary */);
+    this.element.appendChild(mobileConfigButton);
 
     this.element.classList.add('node-frontend');
-  }
-
-  _update() {
-    const config = this._networkDiscoveryConfig.map(item => item.address);
-    this._callback.call(null, config);
   }
 
   _downloadCACertificateButtonClicked() {
       InspectorFrontendHost.openInNewTab('/certs/ca.crt')
   }
-
-  /**
-   * @param {!Adb.NetworkDiscoveryConfig} networkDiscoveryConfig
-   */
-  discoveryConfigChanged(networkDiscoveryConfig) {
-    this._networkDiscoveryConfig = [];
-    this._list.clear();
-    for (const address of networkDiscoveryConfig) {
-      const item = {address: address, port: ''};
-      this._networkDiscoveryConfig.push(item);
-      this._list.appendItem(item, true);
-    }
-  }
-
-  /**
-   * @override
-   * @param {!Adb.PortForwardingRule} rule
-   * @param {boolean} editable
-   * @return {!Element}
-   */
-  renderItem(rule, editable) {
-    const element = createElementWithClass('div', 'network-discovery-list-item');
-    element.createChild('div', 'network-discovery-value network-discovery-address').textContent = rule.address;
-    return element;
-  }
-
-  /**
-   * @override
-   * @param {!Adb.PortForwardingRule} rule
-   * @param {number} index
-   */
-  removeItemRequested(rule, index) {
-    this._networkDiscoveryConfig.splice(index, 1);
-    this._list.removeItem(index);
-    this._update();
-  }
-
-  /**
-   * @override
-   * @param {!Adb.PortForwardingRule} rule
-   * @param {!UI.ListWidget.Editor} editor
-   * @param {boolean} isNew
-   */
-  commitEdit(rule, editor, isNew) {
-    rule.address = editor.control('address').value.trim();
-    if (isNew)
-      this._networkDiscoveryConfig.push(rule);
-    this._update();
-  }
-
-  /**
-   * @override
-   * @param {!Adb.PortForwardingRule} rule
-   * @return {!UI.ListWidget.Editor}
-   */
-  beginEdit(rule) {
-    const editor = this._createEditor();
-    editor.control('address').value = rule.address;
-    return editor;
-  }
-
-  /**
-   * @return {!UI.ListWidget.Editor<!Adb.PortForwardingRule>}
-   */
-  _createEditor() {
-    if (this._editor)
-      return this._editor;
-
-    const editor = new UI.ListWidget.Editor();
-    this._editor = editor;
-    const content = editor.contentElement();
-    const fields = content.createChild('div', 'network-discovery-edit-row');
-    const input = editor.createInput('address', 'text', 'Network address (e.g. localhost:9229)', addressValidator);
-    fields.createChild('div', 'network-discovery-value network-discovery-address').appendChild(input);
-    return editor;
-
-    /**
-     * @param {!Adb.PortForwardingRule} rule
-     * @param {number} index
-     * @param {!HTMLInputElement|!HTMLSelectElement} input
-     * @return {boolean}
-     */
-    function addressValidator(rule, index, input) {
-      const match = input.value.trim().match(/^([a-zA-Z0-9\.\-_]+):(\d+)$/);
-      if (!match)
-        return false;
-      const port = parseInt(match[2], 10);
-      return port <= 65535;
-    }
+  _downloadMobileConfigButtonClicked() {
+      InspectorFrontendHost.openInNewTab('/certs/lantern.mobileconfig')
   }
 };
